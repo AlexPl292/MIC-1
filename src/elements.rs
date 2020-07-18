34f5a1c198a007f6dc382a6f458624cce7010006
Alex Plate <AlexPl292@gmail.com>
2020-07-18 13:05:17 +0300

    Move adder to a different file

---
+++ src/elements.rs
@@ -0,0 +1,8 @@
+fn half_adder(a: bool, b: bool) -> (bool, bool) { (a ^ b, a && b) }
+
+fn adder(a: bool, b: bool, carry_in: bool) -> (bool, bool) {
+    let (sum1, carry1) = half_adder(a, b);
+    let (sum, carry2) = half_adder(sum1, carry_in);
+    let carry_out = carry1 || carry2;
+    (sum, carry_out)
+}
