d3f5ce10936965e22006ec02cd2294b3d17a5aeb
Alex Plate <AlexPl292@gmail.com>
2020-07-29 22:49:59 +0300

    Write tests for alu

--- src/alu.rs
+++ src/alu.rs
@@ -1,5 +1,6 @@
-use crate::bus::{Bus32};
+use crate::bus::Bus32;
 use crate::decoders::decoder_2x4;
+use crate::main_memory::{fast_decode, fast_encode};
 
 fn adder(a: bool, b: bool, carry_in: bool) -> (bool, bool) {
     let (sum1, carry1) = half_adder(a, b);
@@ -31,6 +32,43 @@ impl AluControl {
             inc: code[5],
         }
     }
+
+    fn new() -> AluControl { AluControl { f0: false, f1: false, en_a: false, en_b: false, inv_a: false, inc: false } }
+
+    fn f0(&mut self) -> &mut AluControl {
+        self.f0 = true;
+        self
+    }
+    fn f1(&mut self) -> &mut AluControl {
+        self.f1 = true;
+        self
+    }
+    fn ena(&mut self) -> &mut AluControl {
+        self.en_a = true;
+        self
+    }
+    fn enb(&mut self) -> &mut AluControl {
+        self.en_b = true;
+        self
+    }
+    fn inva(&mut self) -> &mut AluControl {
+        self.inv_a = true;
+        self
+    }
+    fn inc(&mut self) -> &mut AluControl {
+        self.inc = true;
+        self
+    }
+
+    fn alu_b_dec() -> AluControl { AluControl { f0: true, f1: true, en_a: false, en_b: true, inv_a: true, inc: false } }
+    fn alu_b_inc() -> AluControl { AluControl { f0: true, f1: true, en_a: false, en_b: true, inv_a: false, inc: true } }
+    fn alu_sum() -> AluControl { AluControl { f0: true, f1: true, en_a: true, en_b: true, inv_a: false, inc: false } }
+    fn alu_sum_inc() -> AluControl { AluControl { f0: true, f1: true, en_a: true, en_b: true, inv_a: false, inc: true } }
+    fn alu_sub() -> AluControl { AluControl { f0: true, f1: true, en_a: true, en_b: true, inv_a: true, inc: true } }
+    fn alu_and() -> AluControl { AluControl { f0: false, f1: false, en_a: true, en_b: true, inv_a: false, inc: false } }
+    fn alu_or() -> AluControl { AluControl { f0: false, f1: true, en_a: true, en_b: true, inv_a: false, inc: false } }
+    fn alu_b() -> AluControl { AluControl { f0: false, f1: true, en_a: false, en_b: true, inv_a: false, inc: false } }
+    fn alu_a() -> AluControl { AluControl { f0: false, f1: true, en_a: true, en_b: false, inv_a: false, inc: false } }
 }
 
 fn alu_unit(a: bool, b: bool, inv_a: bool, en_a: bool, en_b: bool, carry_in: bool, f0: bool, f1: bool) -> (bool, bool) {
@@ -73,6 +111,92 @@ pub fn alu_32(a: Bus32, b: Bus32, control: AluControl) -> (Bus32, bool, bool) {
     for i in 0..32 {
         z_bit |= result[i];
     }
+    z_bit = !z_bit;
 
     (Bus32::from(result), n_bit, z_bit)
+}
+
+fn alu_32_i(a: i32, b: i32, control: AluControl) -> (i32, bool, bool) {
+    let a_bus = Bus32::from(fast_decode(a));
+    let b_bus = Bus32::from(fast_decode(b));
+
+    let (alu_res, n, z) = alu_32(a_bus, b_bus, control);
+    return (fast_encode(&alu_res.data), n, z);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decrement() {
+        let (res, n, z) = alu_32_i(0, 1, AluControl::alu_b_dec());
+        assert_eq!(0, res);
+        assert_eq!(false, n);
+        assert_eq!(true, z);
+    }
+
+    #[test]
+    fn decrement_1() {
+        let (res, n, z) = alu_32_i(0, 0, AluControl::alu_b_dec());
+        assert_eq!(-1, res);
+        assert_eq!(true, n);
+        assert_eq!(false, z);
+    }
+
+    #[test]
+    fn decrement_2() {
+        let (res, n, z) = alu_32_i(0, 10, AluControl::alu_b_dec());
+        assert_eq!(9, res);
+        assert_eq!(false, n);
+        assert_eq!(false, z);
+    }
+
+    #[test]
+    fn increment() {
+        let (res, n, z) = alu_32_i(0, 10, AluControl::alu_b_inc());
+        assert_eq!(11, res);
+        assert_eq!(false, n);
+        assert_eq!(false, z);
+    }
+
+    #[test]
+    fn increment_1() {
+        let (res, n, z) = alu_32_i(0, -2, AluControl::alu_b_inc());
+        assert_eq!(-1, res);
+        assert_eq!(true, n);
+        assert_eq!(false, z);
+    }
+
+    #[test]
+    fn increment_2() {
+        let (res, n, z) = alu_32_i(0, -1, AluControl::alu_b_inc());
+        assert_eq!(0, res);
+        assert_eq!(false, n);
+        assert_eq!(true, z);
+    }
+
+    #[test]
+    fn sum() {
+        let (res, n, z) = alu_32_i(0, -1, AluControl::alu_sum());
+        assert_eq!(-1, res);
+        assert_eq!(true, n);
+        assert_eq!(false, z);
+    }
+
+    #[test]
+    fn sum_1() {
+        let (res, n, z) = alu_32_i(1, 2, AluControl::alu_sum());
+        assert_eq!(3, res);
+        assert_eq!(false, n);
+        assert_eq!(false, z);
+    }
+
+    #[test]
+    fn sum_2() {
+        let (res, n, z) = alu_32_i(0, 0, AluControl::alu_sum());
+        assert_eq!(0, res);
+        assert_eq!(false, n);
+        assert_eq!(true, z);
+    }
 }
