efc3dca6951cf6e6165b3ef6baa64608d52aacce
Alex Plate <AlexPl292@gmail.com>
2020-08-05 10:46:46 +1000

    Write tests for all8

--- src/shifter.rs
+++ src/shifter.rs
@@ -27,3 +27,43 @@ pub fn sra1(data: Bus32, enabled: bool) -> Bus32 {
     res.data[31] = res.data[30];
     res
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn all_ones() {
+        let bus32 = Bus32::from([true; 32]);
+
+        let shifted = sll8(bus32, true);
+
+        let result = shifted.data;
+        for x in 0..8 {
+            assert_eq!(result[x], false);
+        }
+        for x in 8..32 {
+            assert_eq!(result[x], true);
+        }
+    }
+
+    #[quickcheck]
+    fn ssl8_check(data: Vec<bool>) {
+        let mut res = [false; 32];
+        for x in 0..32 {
+            res[x] = *data.get(x).unwrap_or(&false);
+        }
+
+        let bus32 = Bus32::from(res);
+
+        let shifted = sll8(bus32, true);
+
+        let result = shifted.data;
+        for x in 0..8 {
+            assert_eq!(result[x], false);
+        }
+        for x in 8..32 {
+            assert_eq!(result[x], res[x - 8]);
+        }
+    }
+}
